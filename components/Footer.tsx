3d845f715031d04edf0666e5599a2d4e6ebf72b8
itsviv0 <vivekganeshs12@gmail.com>
2025-04-11 14:09:59 +0530

    Refactor Footer component to use Next.js Link and Image components.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,28 +1,32 @@
 import { Github } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Footer() {
-    return (
-        <footer className="mt-6 sm:mt-12 py-4 sm:py-6 border-t dark:border-gray-800">
-            <div className="max-w-7xl mx-auto px-4 flex items-center justify-center space-x-2 sm:space-x-4 text-gray-600 dark:text-gray-400 text-sm sm:text-base">
-                <a href="/">
-                    <img
-                        src="/branding.svg"
-                        alt="Branding TextDiff"
-                        className="w-5 dark:invert"
-                    />
-                </a>
-                <span className="text-base fonxxt-medium">| </span>
-                <span className="text-base font-medium">Made by</span>
-                <a
-                    href="https://github.com/itsviv0"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="flex items-center hover:text-gray-900 dark:hover:text-gray-200 transition-colors"
-                >
-                    <Github className="w-4 h-4 mr-1" />
-                    itsviv0
-                </a>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="mt-6 sm:mt-12 py-4 sm:py-6 border-t dark:border-gray-800">
+      <div className="max-w-7xl mx-auto px-4 flex items-center justify-center space-x-2 sm:space-x-4 text-gray-600 dark:text-gray-400 text-sm sm:text-base">
+        <Link href="/">
+          <Image
+            src="/branding.svg"
+            alt="Branding TextDiff"
+            width={20}
+            height={20}
+            className="w-5 dark:invert"
+          />
+        </Link>
+        <span className="text-base fonxxt-medium">| </span>
+        <span className="text-base font-medium">Made by</span>
+        <Link
+          href="https://github.com/itsviv0"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center hover:text-gray-900 dark:hover:text-gray-200 transition-colors"
+        >
+          <Github className="w-4 h-4 mr-1" />
+          itsviv0
+        </Link>
+      </div>
+    </footer>
+  );
 }
